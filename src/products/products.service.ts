f187d30e0d652279711b560f0b23afa26cf080e7
Jhossuan <jhossuacamposd86@gmail.com>
2025-01-21 10:04:11 -0500

    transform to microservices and more

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from "@nestjs/common";
+import { HttpStatus, Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from "@prisma/client";
 import { PaginationDto } from "../common";
+import { RpcException } from "@nestjs/microservices";
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -46,7 +47,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     const product = await this.product.findUnique({ where: { id, available: true } })
 
     if(!product){
-      throw new NotFoundException(`Product with id ${id} not found`)
+      throw new RpcException({
+        message: `Product with id ${id} not found`,
+        status: HttpStatus.BAD_REQUEST,
+      })
     }
     return product
   }
